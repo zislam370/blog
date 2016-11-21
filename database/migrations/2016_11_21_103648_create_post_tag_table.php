3f59323a444c8175ded7816a483acd3a49bd195a
Zahidul Islam <z.islam370@gmail.com>
2016-11-21 17:05:01 +0600

    tag update

--- database/migrations/2016_11_21_103648_create_post_tag_table.php
+++ database/migrations/2016_11_21_103648_create_post_tag_table.php
@@ -15,9 +15,8 @@ class CreatePostTagTable extends Migration
         Schema::create('post_tag', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('post_id')->unsigned();
-            $table ->foreign('post_id')->references('id')->on('posts');
             $table->integer('tag_id')->unsigned();
-            $table ->foreign('tag_id')->references('id')->on('tags');
+
         });
     }
 
